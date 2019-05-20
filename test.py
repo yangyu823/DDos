3c46eea2d651a2aa9f55d51203dbdfaae8631b46
Yu Yang <yangyu823@gmail.com>
2019-05-20 14:42:38 +1000

    small update

--- test.py
+++ test.py
@@ -1,6 +1,20 @@
 import sys
+import random
+import os
+import socket
 
+i = 0
+test = random._urandom(1490)
+test2 = bytes(512)
 
-# print("\033[91m check server ip and port \033[0m")
-
-print(sys.argv)
+os.system("figlet DDos Attack");
+# print(test2);
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.sendto(bytes(512), ("127.0.0.1", 8888))
+ip = socket.gethostbyname("yyan823.herokuapp.com")
+# port = 8888
+# while i < 1000000:
+#     sock.sendto(test, ("127.0.0.1", port))
+#     # print(i)
+#     i += 1
+print(ip)
